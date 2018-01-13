51a7c14cb10e937aa5a42df8e670c6078b81d7d4
OwnDroid <mhrvisser21@gmail.com>
2018-01-13 15:55:44 +0100

    create github pages

--- assets/theme/js/script.js
+++ assets/theme/js/script.js
@@ -1,3 +1,4 @@
+!function(){try{document.getElementsByClassName("engine")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.className="engine";a.innerHTML='<a href="https://mobirise.info">Mobirise</a> Mobirise v4.5.4';document.body.insertBefore(a,document.body.childNodes[0])}}();
 (function($){
 
     $.extend($.easing, {
@@ -483,4 +484,4 @@
         }
     }    
 
-})(jQuery);!function(){try{document.getElementsByClassName("engine")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.className="engine";a.innerHTML='<a href="https://mobirise.com">mobirise.com</a> Mobirise v3.12.1';document.body.insertBefore(a,document.body.childNodes[0])}}();
+})(jQuery);
